2482d1edda7a8dffc1e3206f926c76a071841b99
SunChao138 <13853158735@163.com>
2020-07-23 18:29:00 +0800

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -45,7 +45,7 @@ const App = () => {
   return (
     <div className='container'>
       <div className='row center'>
-        <h1 className='white-text'> BMI Tracker </h1>
+        <h1 className='white-text'> BMI 指数 </h1>
       </div>
       <div className='row'>
         <div className='col m12 s12'>
